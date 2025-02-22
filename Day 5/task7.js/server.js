c6b1c872bce9b4497171b0f073df6f009de023d9
sanjay130604 <sanjaysaravanan130604@gmail.com>
2025-02-22 10:45:06 +0530

    Add files via upload

---
+++ Day 5/task7.js/server.js
@@ -0,0 +1,24 @@
+const http = require('http');
+const server = http.createServer((req, res) => {
+  res.setHeader('Content-Type', 'text/html');
+  if (req.url === '/') {
+    res.statusCode = 200;
+    res.end('<h1>Welcome to the Home Page!</h1>');
+  } else if (req.url === '/about') {
+    res.statusCode = 200;
+    res.end('<h1>About Us</h1><p>This is the about page.</p>');
+  } else if (req.url === '/contact') {
+    res.statusCode = 200;
+    res.end('<h1>Contact Us</h1><p>This is the contact page.</p>');
+  } else {
+    res.statusCode = 404;
+    res.end('<h1>Page Not Found</h1><p>The page you are looking for does not exist.</p>');
+  }
+});
+
+const PORT = 4000;
+server.listen(PORT, () => {
+  console.log(`Server is running at http://localhost:${PORT}`);
+});
+
+
